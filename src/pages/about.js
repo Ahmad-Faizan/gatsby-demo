854aa99b2915584cc3c0085c7a70e40177e6e261
Faizan Ahmad <faizan.nasro@gmail.com>
2019-12-29 22:38:06 +0530

    Added an about page

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default() => (
+    <div style={{ color: `teal` }}>
+        <h1>About Page of Gatsby</h1>
+        <p>WoW, such React</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
-
-export default () => <div>Hello world from Gatsby and Faizan!</div>
+// import components from "../components/"
+export default () => (
+    <div style={{ color: `purple` }}>
+        <h1>Hello Gatsby!</h1>
+        <p>Is this Earth?</p>
+        <img src="https://source.unsplash.com/random/1080x720" />
+    </div>
+)
