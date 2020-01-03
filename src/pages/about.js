f7915ae12202b907e1ee5908ce5dc724bc5b4e8a
Faizan Ahmad <faizan.nasro@gmail.com>
2020-01-03 15:29:23 +0530

    Checking out Layout components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,8 @@
 import React from "react"
-
-export default() => (
-    <div style={{ color: `teal` }}>
-        <h1>About Page of Gatsby</h1>
-        <p>WoW, such React</p>
-    </div>
+import Layout from "../components/layout"
+export default () => (
+  <Layout>
+    <h1>About me</h1>
+    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+  </Layout>
 )
